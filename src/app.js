cb273db08da3f54960caa8ec3e51c4547d3c0a52
Jeffrey Tong <trigun0x2@gmail.com>
2018-01-23 20:51:40 -0500

    updated forward channel

--- src/app.js
+++ src/app.js
@@ -3,9 +3,8 @@ var _ = require('underscore-node');
 var fs = require('fs');
 var configFile = fs.readFileSync("config.json");
 var config = JSON.parse(configFile);
-// const LISTEN_CHANNELS = config.channels;
-// Test channel: 
-const LISTEN_CHANNELS = ["401183538856329216"];
+const LISTEN_CHANNELS = config.channels;
+// Test channel:
 const FORWARD_CHANNEL = config.foward_channel;
 
 var bot = new Eris(config.bot_token);
