4979e7f21136a0c92b4557bef7e04c48df45a1fa
Jeffrey Tong <trigun0x2@gmail.com>
2018-01-23 20:50:30 -0500

    updated from channel

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ var _ = require('underscore-node');
 var fs = require('fs');
 var configFile = fs.readFileSync("config.json");
 var config = JSON.parse(configFile);
-const LISTEN_CHANNELS = config.channels;
-// Test channel: 401183538856329216
+// const LISTEN_CHANNELS = config.channels;
+// Test channel: 
+const LISTEN_CHANNELS = ["401183538856329216"];
 const FORWARD_CHANNEL = config.foward_channel;
 
 var bot = new Eris(config.bot_token);
@@ -15,8 +16,10 @@ bot.on("ready", () => {
 
 bot.on('messageCreate', (msg) => {
   if (_.contains(LISTEN_CHANNELS,msg.channel.id)) {
-    console.log(msg.content);
-    bot.createMessage(FORWARD_CHANNEL, msg.content);
+    console.log(msg.channel.name + " - FORWARDED");
+    bot.createMessage(FORWARD_CHANNEL, {content: "@everyone FROM **" + msg.channel.name + "**", disableEveryone: false});
+    let content = {content: msg.content, disableEveryone: false, tts: false};
+    bot.createMessage(FORWARD_CHANNEL, content);
   }
 });
 
